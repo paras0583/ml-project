1fb5578bb75b471fd2a76cd287cf8017c8674cb2
paras <shobhanautiyal0@gmail.com>
2025-06-20 17:39:12 +0530

    model deployed

---
+++ src/pipeline/predict_pipeline.py
@@ -0,0 +1,69 @@
+import sys
+import pandas as pd
+from src.exception import CustomException
+from src.utils import load_object
+
+class PredictPipeline():
+    def __init__(self):
+        pass
+
+class CustomData():
+    def __init__(self , gender: str,
+        race_ethnicity: str,
+        parental_level_of_education,
+        lunch: str,
+        test_preparation_course: str,
+        reading_score: int,
+        writing_score: int):
+
+        self.gender = gender
+
+        self.race_ethnicity = race_ethnicity
+
+        self.parental_level_of_education = parental_level_of_education
+
+        self.lunch = lunch
+
+        self.reading_score = reading_score
+
+        self.writing_score = writing_score
+
+    def get_data_as_dataframe():
+        def get_data_as_data_frame(self):
+        try:
+            custom_data_input_dict = {
+                "gender": [self.gender],
+                "race_ethnicity": [self.race_ethnicity],
+                "parental_level_of_education": [self.parental_level_of_education],
+                "lunch": [self.lunch],
+                "reading_score": [self.reading_score],
+                "writing_score": [self.writing_score],
+            }
+
+            return pd.DataFrame(custom_data_input_dict)
+
+        except Exception as e:
+            raise CustomException(e, sys)
+
+
+class predict(self,features):
+    try:
+        model_path = open.path.join("artifacts","model.pkl")
+        preprocessor_path = open.path.join("artifacts","preprocessor.pkl")
+        print(".....Before Loading the binary file as to bring the objects form the binary file.....")
+        model = load_object(file_path=model_path)
+        preprocessor = load_object(file_path=preprocessor_path)
+        print(".....After loading the pkl files now we can use our previous functions....")
+        data_scaled=preprocessor.transform(features)
+        preds=model.predict(data_scaled)
+        return preds
+
+    except Exception as e:
+        return CustomException(e,sys)
+
+    
+
+
+
+
+
